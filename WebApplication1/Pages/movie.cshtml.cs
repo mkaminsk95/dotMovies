7f1b0575fd7e4625b04e0219aa66d8a09e224fe7
m.kaminsk95 <m.kaminsk95@gmail.com>
2021-05-16 15:22:26 +0100

    GetMovie method added in MovieDBService

--- WebApplication1/Pages/movie.cshtml.cs
+++ WebApplication1/Pages/movie.cshtml.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dotMovies.Models;
+using dotMovies.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace dotMovies.Pages
 {
-    public class movieModel : PageModel
+    public class MovieModel : PageModel
     {
+        public MovieDBService MovieService;
+
+        public Movie Movie { get; private set; }
+
+        public MovieModel(MovieDBService movieService) {
+ 
+            MovieService = movieService;
+        }
+
         public void OnGet()
         {
+            int id = int.Parse((string)RouteData.Values["id"]);
 
+            Movie = MovieService.GetMovie(id);
         }
     }
 }
--- WebApplication1/Services/MovieDBService.cs
+++ WebApplication1/Services/MovieDBService.cs
@@ -9,26 +9,57 @@ using System.Threading.Tasks;
 namespace dotMovies.Services {
     public class MovieDBService {
 
-        private string connectionString = "server=localhost;user id=root;database=dotmoviesdb;allowuservariables=True;password=movieDBpassword";
+        private string _connectionString = "server=localhost;user id=root;database=dotmoviesdb;allowuservariables=True;password=movieDBpassword";
+        private MySqlConnection _connection; 
 
         public MovieDBService(IWebHostEnvironment webHostEnvironment) {
             WebHostEnvironment = webHostEnvironment;
+            _connection = new MySqlConnection(_connectionString);
         }
 
         public IWebHostEnvironment WebHostEnvironment { get; }
 
+        public MySqlDataReader GetMovieDataFromDatabase(string queryString) {
+       
+            _connection.Open();
+
+            MySqlCommand command = _connection.CreateCommand();
+            command.CommandText = queryString;
+
+            MySqlDataReader movieDataReader = command.ExecuteReader();
+            
+            return movieDataReader;
+        }
+
+        public Movie GetMovie(int movieId) {
+
+            Movie movie = new Movie();
+
+            MySqlDataReader moviesReader = GetMovieDataFromDatabase($"SELECT * FROM movies WHERE movieId = {movieId}");
+
+            moviesReader.Read();
+
+            movie.MovieId = moviesReader.GetInt32(0);
+            movie.Title = moviesReader.GetString(1);
+            movie.Year = moviesReader.GetInt16(2);
+            movie.Poster = moviesReader.GetString(3);
+            movie.Director = moviesReader.GetString(4);
+            movie.Screenplay = moviesReader.GetString(5);
+            movie.Runtime = moviesReader.GetTimeSpan(6);
+            movie.Budget = moviesReader.GetInt32(7);
+            movie.Revenue = moviesReader.GetInt32(8);
+            movie.AverageScore = moviesReader.GetFloat(9);
+
+            _connection.Close();
+
+            return movie;
+        }
+
         public List<Movie> GetMovies() {
 
             List<Movie> movies = new List<Movie>();
-            
 
-            MySqlConnection connection = new MySqlConnection(connectionString);
-            connection.Open();
-
-            MySqlCommand command = connection.CreateCommand();
-            command.CommandText = "SELECT * FROM movies ORDER BY averageScore DESC LIMIT 100";
-
-            MySqlDataReader moviesReader = command.ExecuteReader();
+            MySqlDataReader moviesReader = GetMovieDataFromDatabase("SELECT * FROM movies ORDER BY averageScore DESC LIMIT 100");
 
             while(moviesReader.Read()) {
 
@@ -44,7 +75,7 @@ namespace dotMovies.Services {
                 movies.Add(movie);
             }
 
-            connection.Close();
+            _connection.Close();
 
             return movies;
         }
